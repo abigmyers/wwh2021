9dcd2f748f89911f4fb7f78c45a9c86aa5c57c03
Zane Smalley <zane.a.smalls@gmail.com>
2021-02-20 14:05:24 -0500

    Added search api call

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Grid, TextField } from "@material-ui/core";
 import SearchIcon from '@material-ui/icons/Search';
+import { Search } from "@material-ui/icons";
+
+
 
 function SearchBar(props) {
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState(undefined);
+
+  const handleKey = (event) => {
+    if (event.key === "Enter") {
+      search();
+    }
+  };
+
+  const search = () => {
+    if (props.loggedIn) {
+      props.spotifyApi.searchTracks(searchTerm, {"limit":"5"})
+        .then((response) => {
+          setSearchResults(response.tracks);
+          console.log(searchResults);
+        });
+    }
+  };
+
+  const handleChange = (event) => {
+    setSearchTerm(event?.target.value);
+  }
+
   return (
     <div className="SearchBar">
       
@@ -12,7 +39,7 @@ function SearchBar(props) {
             <SearchIcon />
           </Grid>
           <Grid item>
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" onKeyPress={handleKey} onChange={handleChange} />
           </Grid>
         </Grid>
     </div>
