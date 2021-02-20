36a061389ea29a36ecc983aab607ab23490e7cac
Claire Nicholas <clairenicholas@rover-246-91.rovernet.mtu.edu>
2021-02-20 17:30:06 -0500

    Fix player

--- src/Player.js
+++ src/Player.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import SpotifyWebApi from 'spotify-web-api-js';
 import { Paper, Typography } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+
 
 const spotifyApi = new SpotifyWebApi();
 
@@ -8,6 +10,7 @@ function Player(props) {
 	const [title, setTitle] = useState('No song playing');
 	const [artist, setArtist] = useState('No song playing');
 	const [albumCover, setAlbumCover] = useState(null);
+	const [album, setAlbum] = useState("No album");
 	const [currentlyPlaying, setCurrentlyPlaying] = useState(false);
 	const [currentProgress, setCurrentProgress] = useState(0);
 	const [upToDate, setUpToDate] = useState(false);
@@ -37,17 +40,20 @@ function Player(props) {
 						setArtist(response.item?.artists[0].name);
 						setCurrentProgress(response.progress_ms);
 						setTitle(response.item?.name);
+						setAlbum(response.item?.album.name);
 						setAlbumCover(response.item?.album.images[2].url);
 					} else {
 						setArtist('No song playing');
 						setCurrentProgress(0);
 						setTitle('No song playing');
+						setAlbum('No album');
 						setCurrentlyPlaying(false);
 					}
 				});
 			} else {
 				setArtist('No song playing');
 				setTitle('No song playing');
+				setAlbum('No album');
 				setCurrentProgress(0);
 				setCurrentlyPlaying(false);
 			}			
@@ -56,12 +62,26 @@ function Player(props) {
 	return (
 		<div className="Player">
 			{currentlyPlaying && (
-				<Paper variant="outlined" elevation={2}>
-					<Typography variant="subtitle2">Currently Playing:</Typography>
-					<img src={albumCover} alt="album cover"></img>
-					<Typography variant="body1">
-						{title} by {artist}
-					</Typography>
+				<Paper variant="outlined" elevation={2} style={{backgroundColor: "#222222", width: "70%", marginLeft: "15%", marginTop: "2%", marginBottom: "2%", marginRight: "15%"}} >
+					<Grid container alignItems="center" alignContent="space-between" spacing={2}>
+						<Grid item xs={3.5}>
+							<Typography variant="subtitle2" style={{WebkitTextFillColor: "white"}}>Currently Playing:</Typography>
+							<img src={albumCover} alt="album cover" style={{margin: "10%", height: "100px"}}></img>
+						</Grid>
+						<Grid item xs={5}>
+							<Typography variant="body1" style={{WebkitTextFillColor: "white", margin: "2%"}} >
+								Title: {title}
+							</Typography>
+							<Typography variant="body1" style={{WebkitTextFillColor: "white", margin: "2%"}} >
+								Album: {album}
+							</Typography>
+							<Typography variant="body1" style={{WebkitTextFillColor: "white", margin: "2%"}} >
+								Artist: {artist}
+							</Typography>
+						</Grid>
+					</Grid>
+
+
 				</Paper>
 			)}
 		</div>
