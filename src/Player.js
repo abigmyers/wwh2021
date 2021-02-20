82c20089b5742c2cb04ef7bbeab7c7c96d4daaf6
Abby Myers <abigailm@mtu.edu>
2021-02-20 14:52:13 -0500

    Cleaned up SearchBar code

--- src/Player.js
+++ src/Player.js
@@ -1,72 +1,65 @@
-import React, { useState, useEffect } from "react";
-import SpotifyWebApi from "spotify-web-api-js";
-import { Paper, Typography } from "@material-ui/core";
-
+import React, { useState, useEffect } from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
+import { Paper, Typography } from '@material-ui/core';
 
 const spotifyApi = new SpotifyWebApi();
 
 function Player(props) {
+	const [title, setTitle] = useState('No song playing');
+	const [artist, setArtist] = useState('No song playing');
+	const [albumCover, setAlbumCover] = useState(null);
+	const [currentlyPlaying, setCurrentlyPlaying] = useState(false);
 
-  const [title, setTitle] = useState("No song playing");
-  const [artist, setArtist] = useState("No song playing");
-  const [albumCover, setAlbumCover] = useState(null);
-  const [currentlyPlaying, setCurrentlyPlaying] = useState(false);
+	useEffect(() => {
+		if (props.loggedIn) {
+			spotifyApi.getMyCurrentPlayingTrack().then((response) => {
+				if (response.is_playing) {
+					setCurrentlyPlaying(true);
+					setArtist(response.item?.artists[0].name);
+					setTitle(response.item?.name);
+					setAlbumCover(response.item?.album.images[2].url);
+				}
+			});
+		}
+		currentSong();
+	});
 
-  useEffect(() => {
-    if (props.loggedIn) {
-      spotifyApi
-        .getMyCurrentPlayingTrack()
-        .then((response) => {
-          if (response.is_playing) {
-            setCurrentlyPlaying(true);
-            setArtist(response.item?.artists[0].name);
-            setTitle(response.item?.name);
-            setAlbumCover(response.item?.album.images[2].url);
-          }
-        })
-    }
-    currentSong();
-  });
+	const currentSong = () => {
+		setInterval(() => {
+			if (props.loggedIn) {
+				spotifyApi.getMyCurrentPlayingTrack().then((response) => {
+					if (response.is_playing) {
+						setCurrentlyPlaying(true);
+						setArtist(response.item?.artists[0].name);
+						setTitle(response.item?.name);
+						setAlbumCover(response.item?.album.images[2].url);
+					} else {
+						setArtist('No song playing');
+						setTitle('No song playing');
+						setCurrentlyPlaying(false);
+					}
+				});
+			} else {
+				setArtist('No song playing');
+				setTitle('No song playing');
+				setCurrentlyPlaying(false);
+			}
+		}, 10000);
+	};
 
-
-  const currentSong = () => {
-    setInterval(() => {
-      if (props.loggedIn) {
-        spotifyApi
-          .getMyCurrentPlayingTrack()
-          .then((response) => {
-            if (response.is_playing) {
-              setCurrentlyPlaying(true);
-              setArtist(response.item?.artists[0].name);
-              setTitle(response.item?.name);
-              setAlbumCover(response.item?.album.images[2].url);
-            } else {
-              setArtist("No song playing");
-              setTitle("No song playing")
-              setCurrentlyPlaying(false);
-            }
-          })
-      } else {
-        setArtist("No song playing");
-        setTitle("No song playing")
-        setCurrentlyPlaying(false);
-      }
-    }, 10000);
-  }
-
-  return (
-    <div className="Player">
-      {currentlyPlaying && <Paper variant="outlined" elevation={2}>
-        <Typography variant="subtitle2">
-          Currently Playing:
-        </Typography>
-        <img src={albumCover} alt="album cover"></img>
-        <Typography variant="body1">
-          {title} by {artist}
-        </Typography>
-      </Paper>}
-    </div>
-  );
+	return (
+		<div className="Player">
+			{currentlyPlaying && (
+				<Paper variant="outlined" elevation={2}>
+					<Typography variant="subtitle2">Currently Playing:</Typography>
+					<img src={albumCover} alt="album cover"></img>
+					<Typography variant="body1">
+						{title} by {artist}
+					</Typography>
+				</Paper>
+			)}
+		</div>
+	);
 }
 
 export default Player;
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,107 +1,62 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from 'react';
 
-import FormControl from '@material-ui/core/FormControl';
+import { FormControl, Container, Button, TextField, Card, CardContent } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 
-import Container from '@material-ui/core/Container';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-
 function SearchBar(props) {
-  const [searches, showSearches] = useState(false);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [searchResults, setSearchResults] = useState(undefined);
+  const [hasSearched, setHasSearched] = useState(false);
+	const [searchTerm, setSearchTerm] = useState('');
+	const [searchResults, setSearchResults] = useState(undefined);
 
-  const handleKey = (event) => {
-    if (event.key === "Enter") {
-      search();
-    }
-  };
+	const handleKey = (event) => {
+		if (event.key === 'Enter') {
+			search();
+		}
+	};
 
-  const search = () => {
-    if (props.loggedIn) {
-      props.spotifyApi.searchTracks(searchTerm, { "limit": "5" })
-        .then((response) => {
-          setSearchResults(response.tracks);
-          console.log(searchResults);
-        });
-    }
-  };
+	const search = () => {
+		if (props.loggedIn) {
+			props.spotifyApi.searchTracks(searchTerm, { limit: '5' }).then((response) => {
+				setSearchResults(response.tracks);
+				console.log(searchResults);
+        setHasSearched(true);
+			});
+		}
+	};
 
-  const handleChange = (event) => {
-    setSearchTerm(event?.target.value);
-  };
+	const handleChange = (event) => {
+		setSearchTerm(event?.target.value);
+	};
 
-  if(!searches){
-    return (
-      <div className="SearchBar" style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center"
-          }}>
-  
-      <Container maxWidth="sm">
-        <FormControl fullWidth>
-        <TextField
-        id="standard-name"
-        label="Search"
-        InputProps={{startAdornment: <SearchIcon />, endAdornment: <Button variant="contained" onClick={() => showSearches(true)}>Search</Button>}}
-      />
-        </FormControl>
-      </Container>
-      </div>
-    );
-  } else{
-    return(
-      <div className="SearchBar" style={{ 
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center"
-          }}>
-  
-      <Container maxWidth="sm">
-        <FormControl fullWidth>
-          <TextField
-            id="standard-name"
-            label="Search"
-            onKeyPress={handleKey}
-            onChange={handleChange}
-            InputProps={{ startAdornment: <SearchIcon />, endAdornment: <Button variant="contained" onClick={search}>Search</Button> }}
-          />
-        </FormControl>
-
-        <Card >
-          <CardContent>
-            <h3>Song to add</h3>
-          </CardContent>
-        </Card>
-        <Card >
-          <CardContent>
-          <h3>Song to add</h3>
-          </CardContent>
-        </Card>
-        <Card >
-          <CardContent>
-          <h3>Song to add</h3>
-          </CardContent>
-        </Card>
-        <Card >
-          <CardContent>
-          <h3>Song to add</h3>
-          </CardContent>
-        </Card>
-        <Card >
-          <CardContent>
-          <h3>Song to add</h3>
-          </CardContent>
-        </Card>
-      </Container>
-      </div>
-    );
-  }
+	return (
+		<div
+			className="SearchBar"
+			style={{
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}
+		>
+			<Container maxWidth="sm">
+				<FormControl fullWidth>
+					<TextField
+						id="standard-name"
+						label="Search"
+						onKeyPress={handleKey}
+						onChange={handleChange}
+						InputProps={{
+							startAdornment: <SearchIcon />,
+							endAdornment: (
+								<Button variant="contained" onClick={search}>
+									Search
+								</Button>
+							),
+						}}
+					/>
+				</FormControl>
+			</Container>
+		</div>
+	);
 }
 
 export default SearchBar;
