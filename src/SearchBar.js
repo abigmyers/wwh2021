acb55cec68d6cd02eb80a23f797c1578142da679
Zane Smalley <zane.a.smalls@gmail.com>
2021-02-20 14:12:03 -0500

    Fixed merge mistakes

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -2,11 +2,9 @@ import React, { useState } from "react";
 
 import FormControl from '@material-ui/core/FormControl';
 import SearchIcon from '@material-ui/icons/Search';
-import { Search } from "@material-ui/icons";
 
 
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 
@@ -23,7 +21,7 @@ function SearchBar(props) {
 
   const search = () => {
     if (props.loggedIn) {
-      props.spotifyApi.searchTracks(searchTerm, {"limit":"5"})
+      props.spotifyApi.searchTracks(searchTerm, { "limit": "5" })
         .then((response) => {
           setSearchResults(response.tracks);
           console.log(searchResults);
@@ -37,31 +35,22 @@ function SearchBar(props) {
 
   return (
     <div className="SearchBar" style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center"
-        }}>
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
 
-    <Container maxWidth="sm">
-      <FormControl fullWidth>
-      <TextField
-      id="standard-name"
-      label="Search"
-      InputProps={{startAdornment: <SearchIcon />, endAdornment: <Button variant="contained">Search</Button>}}
-    />
-      </FormControl>
-    </Container>
-    
-    
-      
-      <Grid container spacing={1} alignItems="flex-end">
-          <Grid item>
-            <SearchIcon />
-          </Grid>
-          <Grid item>
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" onKeyPress={handleKey} onChange={handleChange} />
-          </Grid>
-        </Grid>
+      <Container maxWidth="sm">
+        <FormControl fullWidth>
+          <TextField
+            id="standard-name"
+            label="Search"
+            onKeyPress={handleKey}
+            onChange={handleChange}
+            InputProps={{ startAdornment: <SearchIcon />, endAdornment: <Button variant="contained" onClick={search}>Search</Button> }}
+          />
+        </FormControl>
+      </Container>
     </div>
   );
 }
