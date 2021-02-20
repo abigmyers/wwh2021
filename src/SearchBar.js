cd6170b178434782e3c38e881ab325c4dd6b80a5
Claire Nicholas <clairenicholas@rover-246-91.rovernet.mtu.edu>
2021-02-20 14:23:02 -0500

    SearchBar changes

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,33 +1,88 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import FormControl from '@material-ui/core/FormControl';
 import SearchIcon from '@material-ui/icons/Search';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 
 function SearchBar(props) {
-  return (
-    <div className="SearchBar" style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center"
-        }}>
+  const [searches, showSearches] = useState(false);
 
-    <Container maxWidth="sm">
-      <FormControl fullWidth>
-      <TextField
-      id="standard-name"
-      label="Search"
-      InputProps={{startAdornment: <SearchIcon />, endAdornment: <Button variant="contained">Search</Button>}}
-    />
-      </FormControl>
-    </Container>
-    
-    
-      
-    </div>
-  );
+  if(!searches){
+    return (
+      <div className="SearchBar" style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+          }}>
+  
+      <Container maxWidth="sm">
+        <FormControl fullWidth>
+        <TextField
+        id="standard-name"
+        label="Search"
+        InputProps={{startAdornment: <SearchIcon />, endAdornment: <Button variant="contained" onClick={() => showSearches(true)}>Search</Button>}}
+      />
+        </FormControl>
+      </Container>
+      </div>
+    );
+  } else{
+    return(
+      <div className="SearchBar" style={{ 
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+          }}>
+  
+      <Container maxWidth="sm">
+        <FormControl fullWidth>
+        <TextField
+        id="standard-name"
+        label="Search"
+        InputProps={{startAdornment: <SearchIcon />, endAdornment: <Button variant="contained" onClick={() => { }}>Search</Button>}}
+      />
+        </FormControl>
+
+        <Card >
+          <CardContent>
+            <h3>Song to add</h3>
+          </CardContent>
+        </Card>
+        <Card >
+          <CardContent>
+          <h3>Song to add</h3>
+          </CardContent>
+        </Card>
+        <Card >
+          <CardContent>
+          <h3>Song to add</h3>
+          </CardContent>
+        </Card>
+        <Card >
+          <CardContent>
+          <h3>Song to add</h3>
+          </CardContent>
+        </Card>
+        <Card >
+          <CardContent>
+          <h3>Song to add</h3>
+          </CardContent>
+        </Card>
+
+
+      </Container>
+      </div>
+
+
+    );
+  }
+
+  
 }
 
 export default SearchBar;
