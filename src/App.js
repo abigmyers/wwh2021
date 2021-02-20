725b6616e1e1d7a6a1b137e668eff606679d404e
Zane Smalley <zane.a.smalls@gmail.com>
2021-02-20 01:38:48 -0500

    Added AppBar on top

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { useState, useEffect } from "react";
+import { AppBar, Button, Toolbar } from "@material-ui/core/"
+import { makeStyles } from '@material-ui/core/styles';
 
 import SpotifyWebApi from "spotify-web-api-js";
+import { Typography } from "@material-ui/core";
 const spotifyApi = new SpotifyWebApi();
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
 
   const getHashParams = () => {
     var hashParams = {};
@@ -26,14 +43,36 @@ function App() {
       if (token) {
         spotifyApi.setAccessToken(token);
         setLoggedIn(true);
+        getUser();
       }
     }
   });
 
+  const getUser = () => {
+    spotifyApi
+      .getMe()
+      .then((response) => {
+        console.log("ran")
+        setUser(response);
+      })
+  }
+
   return (
-    <div className="App">
-      <a href="http://localhost:8888"> Login to Spotify </a>
-      {loggedIn && (<h1>Logged in!</h1>)}
+    <div className={classes.root}>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            App Name TBD
+          </Typography>
+          {!loggedIn &&
+            <Button variant="outlined" color="inherit" aria-label="login" href="http://localhost:8888" >
+              Login to Spotify
+            </Button>
+          }
+          { loggedIn && <img alt="Avatar" src={user?.images[0].url} style={{height:"50px", width:"50px", borderRadius:"50%"}}></img>}
+
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
